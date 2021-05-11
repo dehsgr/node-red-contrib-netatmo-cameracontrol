fe92ff1716ecbb8fd57136fbee00a67d4d8be89d
dehsgr <dehsgr@outlook.com>
2021-05-11 11:21:27 +0200

    Source beautifications

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 module.exports = function(NodeRED) {
   function NetatmoNode(myConfig) {
-      NodeRED.nodes.createNode(this, myConfig);
-      this.userid = myConfig.userid;
-      this.passwd = myConfig.passwd;
-      this.clientid = myConfig.clientid;
-      this.clientsecret = myConfig.clientsecret;
+	  NodeRED.nodes.createNode(this, myConfig);
+	  this.userid = myConfig.userid;
+	  this.passwd = myConfig.passwd;
+	  this.clientid = myConfig.clientid;
+	  this.clientsecret = myConfig.clientsecret;
   }
 
   NodeRED.nodes.registerType("netatmo-config", NetatmoNode, {
-      credentials: {
-          userid: {type: "text"},
-          passwd: {type: "password"},
-          clientid: {type: "text"},
-          clientsecret: {type: "text"}
-      }
+	  credentials: {
+		  userid: {type: "text"},
+		  passwd: {type: "password"},
+		  clientid: {type: "text"},
+		  clientsecret: {type: "text"}
+	  }
   });
 };
--- control.js
+++ control.js
@@ -1,84 +1,84 @@
 module.exports = function(NodeRED) {
-    'use strict';
+	'use strict';
 
-    // ~~~ constructor / destructor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// ~~~ constructor / destructor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    function CameraControl(myNode)
-    {
-        NodeRED.nodes.createNode(this, myNode);
+	function CameraControl(myNode)
+	{
+		NodeRED.nodes.createNode(this, myNode);
 
-        var Platform = this;
-    
-        this.cameraid = myNode.cameraid;
-        this.config = NodeRED.nodes.getNode(myNode.confignode);
+		var Platform = this;
+	
+		this.cameraid = myNode.cameraid;
+		this.config = NodeRED.nodes.getNode(myNode.confignode);
 
-        Platform.on('input', function (myMessage) {
+		Platform.on('input', function (myMessage) {
 			Platform.SendCommand(myMessage);
-        });
-    }
+		});
+	}
 
-    NodeRED.nodes.registerType('netatmo-cameracontrol', CameraControl);
+	NodeRED.nodes.registerType('netatmo-cameracontrol', CameraControl);
 
-    // ~~~ functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// ~~~ functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    CameraControl.prototype.SendCommand = function(myMessage)
-    {
-        var Platform = this;
+	CameraControl.prototype.SendCommand = function(myMessage)
+	{
+		var Platform = this;
 
-        const request = require('request');
-        request.post({
-            url: 'https://api.netatmo.net/oauth2/token',
-            form: {'grant_type': 'password',
-                'client_id': Platform.config.clientid,
-                'client_secret': Platform.config.clientsecret,
-                'username': Platform.config.userid,
-                'password': Platform.config.passwd,
-                'scope': 'read_station read_thermostat write_thermostat read_camera write_camera access_camera read_presence access_presence write_presence read_homecoach'
-            }
-        }, function(myError, myResponse, myBody) {
-            if(myError) {
-                Platform.warn('There was an Error authenticating: ' + myError);
-            } else {
-                request.get('https://api.netatmo.net/api/gethomedata?access_token=' + JSON.parse(myResponse.body).access_token + '&size=50',
-                function(myError, myResponse, myBody) {
-                    if(myError) {
-                        Platform.warn('There was an Error getting Camera Data: ' + myError);
-                    } else {
+		const request = require('request');
+		request.post({
+			url: 'https://api.netatmo.net/oauth2/token',
+			form: {'grant_type': 'password',
+				'client_id': Platform.config.clientid,
+				'client_secret': Platform.config.clientsecret,
+				'username': Platform.config.userid,
+				'password': Platform.config.passwd,
+				'scope': 'read_station read_thermostat write_thermostat read_camera write_camera access_camera read_presence access_presence write_presence read_homecoach'
+			}
+		}, function(myError, myResponse, myBody) {
+			if(myError) {
+				Platform.warn('There was an Error authenticating: ' + myError);
+			} else {
+				request.get('https://api.netatmo.net/api/gethomedata?access_token=' + JSON.parse(myResponse.body).access_token + '&size=50',
+				function(myError, myResponse, myBody) {
+					if(myError) {
+						Platform.warn('There was an Error getting Camera Data: ' + myError);
+					} else {
 						try {
 							var hs = JSON.parse(myResponse.body).body.homes;
 						}
 						catch (e) {
 							Platform.warn('There was an Error getting Camera Data: ' + myResponse.body);
 						}
-                        for (var h = 0; h < (hs || []).length; h++) {
-                            var cs = hs[h].cameras;
-                            for (var c = 0; c < cs.length; c++) {
-                                if (cs[c].id.toLowerCase() === Platform.cameraid.toLowerCase()) {
-                                    if ('light' in myMessage.payload) {
-                                        Platform.log('Setting Cameras Light Status to \'' + myMessage.payload.light + '\'');
-                                        request.get(cs[c].vpn_url + '/command/floodlight_set_config?config={"mode": "' + myMessage.payload.light + '"}',
-                                        function(myError, myResponse, myBody) {
-                                            if(myError) {
-                                                Platform.warn('There was an Error setting Cameras Light Status: ' + myError);
-                                            }
-                                        });
-                                    }
+						for (var h = 0; h < (hs || []).length; h++) {
+							var cs = hs[h].cameras;
+							for (var c = 0; c < cs.length; c++) {
+								if (cs[c].id.toLowerCase() === Platform.cameraid.toLowerCase()) {
+									if ('light' in myMessage.payload) {
+										Platform.log('Setting Cameras Light Status to \'' + myMessage.payload.light + '\'');
+										request.get(cs[c].vpn_url + '/command/floodlight_set_config?config={"mode": "' + myMessage.payload.light + '"}',
+										function(myError, myResponse, myBody) {
+											if(myError) {
+												Platform.warn('There was an Error setting Cameras Light Status: ' + myError);
+											}
+										});
+									}
 
-                                    if ('monitoring' in myMessage.payload) {
-                                        Platform.log('Setting Cameras Monitoring Status to \'' + myMessage.payload.monitoring + '\'');
-                                        request.get(cs[c].vpn_url + '/command/changestatus?status=' + myMessage.payload.monitoring,
-                                        function(myError, myResponse, myBody) {
-                                            if(myError) {
-                                                Platform.warn('There was an Error setting Cameras Monitoring Status: ' + myError);
-                                            }
-                                        });
-                                    }
-                                }
-                            }
-                        }
-                    }
-                });
-            }
-        });           
-    }
+									if ('monitoring' in myMessage.payload) {
+										Platform.log('Setting Cameras Monitoring Status to \'' + myMessage.payload.monitoring + '\'');
+										request.get(cs[c].vpn_url + '/command/changestatus?status=' + myMessage.payload.monitoring,
+										function(myError, myResponse, myBody) {
+											if(myError) {
+												Platform.warn('There was an Error setting Cameras Monitoring Status: ' + myError);
+											}
+										});
+									}
+								}
+							}
+						}
+					}
+				});
+			}
+		});		   
+	}
 };
